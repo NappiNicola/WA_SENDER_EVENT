5a4396aaf8ec17f6879fa58b91a570014a81c860
NicolaNappi <nicolanappi@live.it>
2025-10-22 15:27:42 +0200

    Add automatic config.json

--- config_loader.py
+++ config_loader.py
@@ -1,20 +1,23 @@
 import json
-import os
-import sys
+from pathlib import Path
 
+CONFIG_FILE = Path("config.json")
+
+DEFAULT_CONFIG = {
+    "API_URL": "",
+    "MY_PHONE": ""
+}
 
 def load_config():
-    """Carica il file di configurazione JSON"""
-    base_dir = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))  # compatibile anche con PyInstaller
-    config_path = os.path.join(base_dir, "config.json")
+    if not CONFIG_FILE.exists():
+        with open(CONFIG_FILE, "w") as f:
+            json.dump(DEFAULT_CONFIG, f, indent=4)
+        return DEFAULT_CONFIG
 
-    if not os.path.exists(config_path):
-        raise FileNotFoundError(f"File di configurazione non trovato: {config_path}")
-
-    with open(config_path, "r", encoding="utf-8") as f:
+    with open(CONFIG_FILE, "r") as f:
         return json.load(f)
 
-# Carichiamo i valori
 config = load_config()
+
 API_URL = config["API_URL"]
 MY_PHONE = config["MY_PHONE"]
